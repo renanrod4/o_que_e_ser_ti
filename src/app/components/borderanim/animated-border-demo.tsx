1f0560cf6c1d11a8e718202879025575cb0ca877
renanrod4 <renanrdemeneses@gmail.com>
2024-11-25 11:42:22 -0300

    Adding humor to text

--- src/app/components/borderanim/animated-border-demo.tsx
+++ src/app/components/borderanim/animated-border-demo.tsx
@@ -9,7 +9,7 @@ export function AnimatedBorderDemo() {
       from="#6F2DBD"
       stop="#6F2DBD"
     >
-      <CardItem text={"Tecnologia da Informação (T.I) é o conjunto de técnicas, ferramentas e sistemas usados para criar, gerenciar e compartilhar informações.\n  Ela envolve hardware, software, redes e a análise de dados, sendo fundamental em quase todas as áreas modernas."} title="O que é T.I?" className="backdrop-blur-3xl bg-darker textContent card" />
+      <CardItem text={"Tecnologia da Informação (T.I) é o conjunto de técnicas, ferramentas e sistemas usados para criar, gerenciar e compartilhar informações.\n hardware, software, redes e análise de dados, T.I. é o motor invisível que move o mundo moderno — e também o culpado quando \"o sistema tá fora do ar\"."} title="O que é T.I?" className="backdrop-blur-3xl bg-darker textContent card" />
     </AnimatedBorder>
   );
 }
